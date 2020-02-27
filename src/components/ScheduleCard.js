a5b1ed821819e27d27f349053b28c30ac4a6ea29
kimminji <alswlk0306@gmail.com>
2020-02-27 19:01:38 +0900

    Change LabelColors logic

--- src/components/ScheduleCard.js
+++ src/components/ScheduleCard.js
@@ -1,31 +1,43 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
+import { darken } from 'polished';
 
 const LabelColors = css`
-  ${props => 
-    props.value ==='To do' &&
-    css`
-      background: #EB5A46;
+  ${props => {
+    if(props.value === 'To do'){
+      return css`
+        background: #EB5A46;
+
+        &:hover {
+          background: ${darken(0.2, '#EB5A46')}
+        }
+      `
+    }
+    else if(props.value === 'Complete'){
+      return css`
+        background: #61BD4F;
+        padding-right: 16px;
+
+        &:hover {
+          background: ${darken(0.1, '#61BD4F')}
+        }
+      `
+    }
+    else if(props.value === 'Progress'){
+      return css`
+        background: #F2D600;
+        padding-right: 8px;
+
+        &:hover {
+          background: ${darken(0.1, '#F2D600')}
+        }
     `
+    }
   }
-  ${props => 
-    props.value ==='Complete' &&
-    css`
-      background: #61BD4F;
-      padding-right: 16px;
-    `
-  }
-  ${props => 
-    props.value ==='Progress' &&
-    css`
-      background: #F2D600;
-      padding-right: 8px;
-    `
-  }
+}
 `;
 
 const ListCards = styled.div`
-  background: none;
   display: flex;
   flex-direction: column;
 `;
@@ -39,6 +51,10 @@ const Cards = styled.div`
   padding: 8px;
   margin-bottom: 8px;
   border-bottom: 1px solid #b2b9c5;
+
+  &:hover {
+    background: ${darken(0.1, '#ffffff')};
+  }
 `;
 
 const CardLabel = styled.div`
@@ -63,8 +79,8 @@ function ScheduleCard({cards}){
   return (
     <ListCards>
       {cards.map(card => (
-        <Cards>
-          <CardLabel key={card.id} id={card.id} value={card.value}>{card.value}</CardLabel>
+        <Cards key={card.id} id={card.id}>
+          <CardLabel value={card.value}>{card.value}</CardLabel>
           <CardText>{card.text}</CardText>
         </Cards>
       ))}
